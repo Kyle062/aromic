1219147a6871532d1eb8a9386f877e48910a885b
Kyle Bernard Trimidal <kylealba0624@gmail.com>
2026-04-12 11:22:39 +0800

    adding the login page

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,20 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { 
+  IonContent, 
+  IonInput, 
+  IonButton, 
+  IonIcon, 
+  IonCheckbox, 
+  IonLabel 
+} from '@ionic/angular/standalone';
+import { RouterLink } from '@angular/router';
+import { addIcons } from 'ionicons';
+import { 
+  logoFacebook, 
+  logoInstagram, 
+  logoLinkedin, 
+  logoGoogle, 
+  eyeOutline, 
+  eyeOffOutline 
+} from 'ionicons/icons';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonContent, IonInput, IonButton, IonIcon, 
+    IonCheckbox, IonLabel, CommonModule, FormsModule, RouterLink
+  ]
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
+  showPassword = false;
+  passwordType = 'password';
+  passwordIcon = 'eye-off-outline';
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    addIcons({ 
+      logoFacebook, logoInstagram, logoLinkedin, 
+      logoGoogle, eyeOutline, eyeOffOutline 
+    });
   }
 
+  togglePassword() {
+    this.showPassword = !this.showPassword;
+    this.passwordType = this.showPassword ? 'text' : 'password';
+    this.passwordIcon = this.showPassword ? 'eye-outline' : 'eye-off-outline';
+  }
 }
--- src/app/splash/splash.page.ts
+++ src/app/splash/splash.page.ts
@@ -1,20 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { 
+  IonContent, 
+  IonButton, 
+  IonIcon 
+} from '@ionic/angular/standalone'; // Import only what you actually use
+import { RouterLink } from '@angular/router';
+import { addIcons } from 'ionicons'; // Required to make icons work in standalone
+import { logoFacebook, logoInstagram, logoLinkedin } from 'ionicons/icons'; 
 
 @Component({
   selector: 'app-splash',
   templateUrl: './splash.page.html',
   styleUrls: ['./splash.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonContent, 
+    IonButton, 
+    IonIcon, 
+    CommonModule, 
+    FormsModule, 
+    RouterLink
+  ]
 })
-export class SplashPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class SplashPage {
+  constructor() {
+    // This registers the icons so the app can find them
+    addIcons({ logoFacebook, logoInstagram, logoLinkedin });
   }
-
 }
