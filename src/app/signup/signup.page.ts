a172ffab8695e0002fe76f1a325b6fe3298dbd4b
Kyle Bernard Trimidal <kylealba0624@gmail.com>
2026-04-12 14:46:04 +0800

    fixing the design

--- src/app/signup/signup.page.ts
+++ src/app/signup/signup.page.ts
@@ -1,20 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonicModule } from '@ionic/angular'; // Brings in ion-input, ion-button, etc.
+import { RouterModule } from '@angular/router'; // Required for routerLink="/login"
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.page.html',
   styleUrls: ['./signup.page.scss'],
-  standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  standalone: true, // Tells Angular this component manages its own dependencies
+  imports: [
+    CommonModule, 
+    FormsModule, 
+    IonicModule, 
+    RouterModule
+  ] 
 })
-export class SignupPage implements OnInit {
+export class SignupPage {
+  
+  // First Password Field
+  passwordType: string = 'password';
+  passwordIcon: string = 'eye-off-outline';
 
-  constructor() { }
+  // Confirm Password Field
+  confirmPasswordType: string = 'password';
+  confirmPasswordIcon: string = 'eye-off-outline';
 
-  ngOnInit() {
+  constructor() {}
+
+  togglePassword() {
+    if (this.passwordType === 'password') {
+      this.passwordType = 'text';
+      this.passwordIcon = 'eye-outline';
+    } else {
+      this.passwordType = 'password';
+      this.passwordIcon = 'eye-off-outline';
+    }
   }
 
+  toggleConfirmPassword() {
+    if (this.confirmPasswordType === 'password') {
+      this.confirmPasswordType = 'text';
+      this.confirmPasswordIcon = 'eye-outline';
+    } else {
+      this.confirmPasswordType = 'password';
+      this.confirmPasswordIcon = 'eye-off-outline';
+    }
+  }
 }
