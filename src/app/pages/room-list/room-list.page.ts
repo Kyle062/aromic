4cb82e70142cae4e1648f48e5e2129f843d51e32
Kyle Bernard Trimidal <kylealba0624@gmail.com>
2026-04-20 10:10:39 +0800

    initializing the design

--- src/app/pages/room-list/room-list.page.ts
+++ src/app/pages/room-list/room-list.page.ts
@@ -1,20 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {
+  IonContent,
+  IonIcon,
+  IonFab,
+  IonFabButton,
+} from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import {
+  arrowBackOutline,
+  createOutline,
+  trashOutline,
+  add,
+} from 'ionicons/icons';
 
 @Component({
   selector: 'app-room-list',
   templateUrl: './room-list.page.html',
   styleUrls: ['./room-list.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonContent,
+    IonIcon,
+    IonFab,
+    IonFabButton,
+    CommonModule,
+    FormsModule,
+  ],
 })
 export class RoomListPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    // Register the icons so they render in standalone mode
+    addIcons({ arrowBackOutline, createOutline, trashOutline, add });
   }
 
+  ngOnInit() {}
 }
