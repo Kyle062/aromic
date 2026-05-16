01b47872c363b1d24edd0583ab130d38f0988042
Kyle Bernard Trimidal <kylealba0624@gmail.com>
2026-05-17 01:39:25 +0800

    about page

--- src/app/pages/about/about.page.ts
+++ src/app/pages/about/about.page.ts
@@ -1,20 +1,59 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonIcon, IonBackButton } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import {
+  arrowBackOutline,
+  menuOutline,
+  bulbOutline,
+  compassOutline,
+  eyeOutline,
+  cubeOutline,
+  layersOutline,
+  cameraOutline,
+  homeOutline,
+  saveOutline,
+  peopleOutline,
+  logoInstagram,
+  logoFacebook,
+  mailOutline,
+  globeOutline,
+  arrowBack,
+} from 'ionicons/icons';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.page.html',
   styleUrls: ['./about.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonIcon, IonBackButton, CommonModule, FormsModule],
 })
 export class AboutPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    addIcons({
+      arrowBackOutline,
+      menuOutline,
+      bulbOutline,
+      compassOutline,
+      eyeOutline,
+      cubeOutline,
+      layersOutline,
+      cameraOutline,
+      homeOutline,
+      saveOutline,
+      peopleOutline,
+      logoInstagram,
+      logoFacebook,
+      mailOutline,
+      globeOutline,
+      arrowBack,
+    });
   }
 
+  ngOnInit() {}
+
+  openMenu() {
+    console.log('Menu opened');
+  }
 }
